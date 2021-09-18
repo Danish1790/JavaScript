6bfb5aedc78d565233050994651acc964b65d5f7
danish <danish@gmail.com>
2021-09-18 07:22:52 -0700

    website 1 added now

---
+++ website1/boot.js
@@ -0,0 +1,10 @@
+// var time = 0
+// var timer = setInterval(function(){
+//   time+=2
+//   console.log(time + "seconds have passsed");
+//   if(time>6){
+//     clearInterval(timer);
+//   }
+// },2000)
+console.log(__dirname);
+console.log(__filename);
